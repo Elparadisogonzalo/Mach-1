fa913d023228517794caa9fa88d8b47e6e6c0eec
Koagonzalo <genyoungclip@gmail.com>
2025-05-06 23:59:16 +0100

    Create config.js
    Signed-off-by: Koagonzalo <genyoungclip@gmail.com>

---
+++ src/src/config.js
@@ -0,0 +1,15 @@
+export const config = {
+  network: {
+    name: "Ethereum Mainnet",
+    chainId: 1,
+    rpcUrl: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID", // Replace with your Infura or Alchemy endpoint
+    blockExplorer: "https://etherscan.io"
+  },
+  wallet: {
+    address: "0x802ba6A112F4A7BBbC2d63c8EF4bC14DFCBe6245"
+  },
+  contract: {
+    address: "0x4E8C73E7f243D12B7A5571200609523A4890beff",
+    abi: [] // Replace with actual ABI
+  }
+};
